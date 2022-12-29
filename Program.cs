f6d78ac50cac8e12b56c376d544338bbf0498e6a
Ahmed2469 <121171225+Ahmed2469@users.noreply.github.com>
2022-12-30 00:18:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Project
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+int x, y, z = 0;
+
+
+
+            Console.WriteLine("n1:-");
+
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("n2:-");
+
+            y = int.Parse(Console.ReadLine());
+
+ 
+
+            for (int i = x; i < y; i++)
+
+            {
+
+                for (int j = 1; j < y; j++)
+
+                {
+
+                    if (i % j == 0)
+
+                    {
+
+                        z++;
+
+                    }
+
+                }
+
+                if (z == 2)
+
+                {
+
+                    Console.WriteLine(i);
+
+                }
+
+                z = 0;
+
+            }
+
+            Console.ReadKey();
+		}
+	}
+}
