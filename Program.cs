d445744c5ff170d7093161ef4dcaeb8bb13130a6
Ahmed2469 <121171225+Ahmed2469@users.noreply.github.com>
2022-12-30 00:35:25 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,33 @@
 ﻿using System;
 using System.Linq;
 
-namespace Project
+namespace Project_2
 {
 	public static class Program
 	{
 		public static void Main()
 		{
-int x, y, z = 0;
-
-
-
-            Console.WriteLine("n1:-");
-
-            x = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("n2:-");
-
-            y = int.Parse(Console.ReadLine());
-
- 
-
-            for (int i = x; i < y; i++)
-
-            {
-
-                for (int j = 1; j < y; j++)
-
-                {
-
-                    if (i % j == 0)
-
-                    {
-
-                        z++;
-
-                    }
-
-                }
-
-                if (z == 2)
-
-                {
-
-                    Console.WriteLine(i);
-
-                }
-
-                z = 0;
-
-            }
-
+int startingNumber, endingNumbeer;  
+            Console.WriteLine("Get All Perfect In Range of Between two Number");  
+            Console.Write("Enter Starting Number : ");  
+            startingNumber = int.Parse(Console.ReadLine());  
+            Console.Write("Enter Ending Number : ");  
+            endingNumbeer = int.Parse(Console.ReadLine());  
+  
+            Console.WriteLine("Below are the perfect number between " + startingNumber + " and " + endingNumbeer);  
+               
+            for (int i = startingNumber; i <= endingNumbeer; i++)  
+            {  
+                decimal sum = 0;  
+                for (int j = 1; j < i; j++)  
+                {  
+                    if (i % j == 0)  
+                        sum = sum + j;  
+                }  
+                if (sum == i)  
+                    Console.WriteLine("\t" +i);  
+            }  
+  
             Console.ReadKey();
 		}
 	}
